34aa5b3f853d4c711b34d4892b54f59ff51c31d8
Java <javaj200222@gmail.com>
2022-05-23 16:23:18 +0500

    JS-Store20

---
+++ script/scriptcard.js
@@ -0,0 +1,22 @@
+let image1 = document.getElementById("image1");
+let image2 = document.getElementById("image2");
+let image3 = document.getElementById("image3");
+let image4 = document.getElementById("image4");
+
+let changeImg = document.getElementById("changeImg");
+
+function images1() {
+    changeImg.src = image1.src;
+}
+
+function images2() {
+    changeImg.src = image2.src;
+}
+
+function images3() {
+    changeImg.src = image3.src;
+}
+
+function images4() {
+    changeImg.src = image4.src;
+}
